bcf3b0f45f6c0ebdb4c144395e47d92a1d07f407
Andrey Malov <58001412+andreymlv@users.noreply.github.com>
2021-03-30 23:40:23 +0300

    Qt added

--- source/main.cpp
+++ source/main.cpp
@@ -1,42 +1,14 @@
-/**
-* \file
-* The most simple example.
-* 
-*/
 
-#include <curlpp/cURLpp.hpp>
-#include <curlpp/Easy.hpp>
-#include <curlpp/Options.hpp>
+#include <QtWidgets>
 
-using namespace curlpp::options;
-
-int main(int, char **)
+int main(int argc, char *argv[])
 {
-    try
-    {
-        // That's all that is needed to do cleanup of used resources (RAII style).
-        curlpp::Cleanup myCleanup;
+    QApplication app(argc, argv);
 
-        // Our request to be sent.
-        curlpp::Easy myRequest;
+    QWidget window;
+    window.resize(320, 240);
+    window.show();
+    window.setWindowTitle("Top-level widget");
 
-        // Set the URL.
-        myRequest.setOpt<Url>("http://example.com");
-
-        // Send request and get a result.
-        // By default the result goes to standard output.
-        myRequest.perform();
-    }
-
-    catch (curlpp::RuntimeError &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-
-    catch (curlpp::LogicError &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-
-    return 0;
+    return app.exec();
 }
